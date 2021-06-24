ccd5425bfa0e59b119565947ac8cfaf96191e6da
Saif <sshussaini96@gmail.com>
2021-06-24 17:26:55 -0400

    removed css and added psuedo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Hooking up API call
+// Grabbing search function from API to allow user to input keyword
+// https://app.ticketmaster.com/discovery/v2/events.json?
+// parameters: apikey, keyword
+// animation/image on isLoading before displaying search
+
+// component to display search results
+
+// display all the events with price-range, date, name, image, url to selected event site
+
+// Form for creating list
+// Text input with placeholder "Create a list" that gets replaced with name
+// Allow user to enter a budget and name of the list
+// Lists stack on top of each other that can be minimized with a button
+// Store name and budget amount into its own database on firebase
+
+// On all the events displayed, have a button for each event where you can add it to the list
+
+
+
+
+
+
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hello</p>
     </div>
   );
 }
