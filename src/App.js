0415430b7f9637bd342f8a70fc68f29a8bdb9d6d
Brandon <brandoncartergdev@gmail.com>
2021-06-24 17:34:14 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Allow user to create list
+//list name and budget
+
+//stretch goals
+//filter through images to decide which pic to use
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   
+
+   $.ajax({
+      type: "GET",
+      url: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=LTtkh2NXZOyGcG6HGOASJH8KgZ4JiKGX",
+      async: true,
+      dataType: "json",
+      success: function (json) {
+         console.log(json);
+         // Parse the response.
+         // Do other things.
+      },
+      error: function (xhr, status, err) {
+         // This time, we do not end up here!
+      }
+   });
+
+
+   return (
+      <div className="App">
+
+         {
+
+         }
+
+
+      </div>
+   );
 }
 
 export default App;
